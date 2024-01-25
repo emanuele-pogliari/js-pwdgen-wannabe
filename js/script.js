12edd352ddcbd554423907a5a4fbb89c04e0fa66
emanuele-pogliari <manu.pogliari@gmail.com>
2024-01-25 17:46:43 +0100

    fix line-height. Exercise completed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-const userName = prompt("Come ti chiami?");
-const userSurname = prompt("Qual'è il tuo cognome?");
-const userColor = prompt("Qual'è il tuo colore preferito?");
+// const userName = prompt("Come ti chiami?");
+// const userSurname = prompt("Qual'è il tuo cognome?");
+// const userColor = prompt("Qual'è il tuo colore preferito?");
 
-let pwd = `${userName}${userSurname}${userColor}${23}`;
+// let pwd = `${userName}${userSurname}${userColor}${23}`;
 
-document.getElementById("name").innerHTML = userName;
-document.getElementById("surname").innerHTML = userSurname;
-document.getElementById("color").innerHTML = userColor;
-document.getElementById("password").innerHTML = pwd;
+// document.getElementById("name").innerHTML = userName;
+// document.getElementById("surname").innerHTML = userSurname;
+// document.getElementById("color").innerHTML = userColor;
+// document.getElementById("password").innerHTML = pwd;
