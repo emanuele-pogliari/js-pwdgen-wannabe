d4f0fda946d50052f21a581a8f80d0f850b269bd
emanuele-pogliari <manu.pogliari@gmail.com>
2024-01-25 17:24:39 +0100

    bonus: css styl added. Excercise completed

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const userName = prompt("Come ti chiami?");
 const userSurname = prompt("Qual'è il tuo cognome?");
 const userColor = prompt("Qual'è il tuo colore preferito?");
 
-let pwd = `${userName}${userSurname}${userColor}23`;
+let pwd = `${userName}${userSurname}${userColor}${23}`;
 
+document.getElementById("name").innerHTML = userName;
+document.getElementById("surname").innerHTML = userSurname;
+document.getElementById("color").innerHTML = userColor;
 document.getElementById("password").innerHTML = pwd;
